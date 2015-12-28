40de652ea54acc2e59d8ed32b0c8b97afb111ef5
Ellen Shapiro <designatednerd@gmail.com>
2015-12-28 17:30:55 -0600

    Consolidate selecting a game on a given day

--- GoCubsUITests/GoCubsUITests.swift
+++ GoCubsUITests/GoCubsUITests.swift
@@ -49,27 +49,35 @@ class GoCubsUITests: XCTestCase {
     }
   }
   
+  func selectGameOnDate(app: XCUIApplication,
+    date: String,
+    withVersusString versus: String) {
+      app.tables[AccessibilityIdentifier.GamesTableview.rawValue]
+        .cells
+        .containingType(.StaticText, identifier:date)
+        .staticTexts[versus]
+        .tap()
+  }
+  
   //MARK: - Actual Tests
   
   func testKnownPostseasonWin() {
     let app = XCUIApplication()
     
     //Select october 10 game
-    let tablesQuery = app.tables
-    tablesQuery.cells
-      .containingType(.StaticText, identifier:"Oct 10")
-      .staticTexts["Cubs vs. Cardinals"]
-      .tap()
+    self.selectGameOnDate(app,
+      date: "Oct 10",
+      withVersusString: "Cubs vs. Cardinals")
     
     //Cubs beat the cardinals 6-3 in this game
-    checkResult(app,
+    self.checkResult(app,
       expectedWinningTeamName: "CUBS",
       expectedWinningTeamScore: "6",
       expectedLosingTeamName: "CARDINALS",
       expectedLosingTeamScore: "3")
     
     //Verify posteason verbiage
-    checkIsPostseason(app, isPostseason: true)
+    self.checkIsPostseason(app, isPostseason: true)
     
     //Is the W showing
     XCTAssertTrue(app.staticTexts[AccessibilityLabel.cubsWin].exists)
@@ -79,22 +87,20 @@ class GoCubsUITests: XCTestCase {
     let app = XCUIApplication()
     
     //Select October 9 game
-    let cubsVsCardinalsStaticText = app
-      .tables[AccessibilityIdentifier.GamesTableview.rawValue]
-      .cells
-      .containingType(.StaticText, identifier:"Oct 9")
-      .staticTexts["Cubs vs. Cardinals"]
-    
-    cubsVsCardinalsStaticText.tap()
+    self.selectGameOnDate(app,
+      date: "Oct 9",
+      withVersusString: "Cubs vs. Cardinals")
     
     //Cubs lost to the cardinals 4-0 in this game
-    checkResult(app,
+    self.checkResult(app,
       expectedWinningTeamName: "CARDINALS",
       expectedWinningTeamScore: "4",
       expectedLosingTeamName: "CUBS",
       expectedLosingTeamScore: "0")
     
-    checkIsPostseason(app, isPostseason: true)
+    self.checkIsPostseason(app, isPostseason: true)
+    
+    //Is the L showing?
     XCTAssertTrue(app.staticTexts[AccessibilityLabel.cubsLose].exists)
   }
   
@@ -102,53 +108,51 @@ class GoCubsUITests: XCTestCase {
     let app = XCUIApplication()
     
     //Select October 4 game
-    app.tables[AccessibilityIdentifier.GamesTableview.rawValue]
-      .cells
-      .containingType(.StaticText, identifier:"Oct 4")
-      .staticTexts["Cubs vs. Brewers"]
-      .tap()
+    self.selectGameOnDate(app,
+      date: "Oct 4",
+      withVersusString: "Cubs vs. Brewers")
     
     //Cubs beat the brewers 3-1 in this game
-    checkResult(app,
+    self.checkResult(app,
       expectedWinningTeamName: "CUBS",
       expectedWinningTeamScore: "3",
       expectedLosingTeamName: "BREWERS",
       expectedLosingTeamScore: "1")
     
-    checkIsPostseason(app, isPostseason: false)
+    self.checkIsPostseason(app, isPostseason: false)
     
     XCTAssertTrue(app.staticTexts[AccessibilityLabel.cubsWin].exists)
   }
   
   func testKnownRegularSeasonLoss() {
     let app = XCUIApplication()
-    let listOfCubsGamesTable = app.tables["List of cubs games"]
-    
+
     //Select August 29 game
-    listOfCubsGamesTable.cells.containingType(.StaticText, identifier:"Aug 29").staticTexts["Cubs vs. Dodgers"].tap()
+    self.selectGameOnDate(app,
+      date: "Aug 29",
+      withVersusString: "Cubs vs. Dodgers")
     
-    //Cubs lost to the Dodgers 4-0 in this game
-    checkResult(app,
+    //Cubs lost to the Dodgers 5-2 in this game
+    self.checkResult(app,
       expectedWinningTeamName: "DODGERS",
       expectedWinningTeamScore: "5",
       expectedLosingTeamName: "CUBS",
       expectedLosingTeamScore: "2")
     
-    checkIsPostseason(app, isPostseason: false)
+    self.checkIsPostseason(app, isPostseason: false)
     XCTAssertTrue(app.staticTexts[AccessibilityLabel.cubsLose].exists)
   }
   
   func testKnownRegularSeasonPostponement() {
-    
     let app = XCUIApplication()
-    let listOfCubsGamesTable = app.tables["List of cubs games"]
     
-    //Go to September 10 game
-    let cubsVsPhilliesStaticText = listOfCubsGamesTable.cells.containingType(.StaticText, identifier:"Sep 10").childrenMatchingType(.StaticText).matchingIdentifier("Cubs vs. Phillies").elementBoundByIndex(0)
-    cubsVsPhilliesStaticText.tap()
+    //Select to September 10 game
+    self.selectGameOnDate(app,
+      date: "Sep 10",
+      withVersusString: "Cubs vs. Phillies")
     
     //This game was rained out.
-    checkResult(app,
+    self.checkResult(app,
       expectedWinningTeamName: "CUBS", //Cubs "Win" by default when a game is cancelled.
       expectedWinningTeamScore: "-",
       expectedLosingTeamName: "PHILLIES",
